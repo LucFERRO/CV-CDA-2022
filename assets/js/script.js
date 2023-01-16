5bda7c1c3572737ab74c26a0ebe453d57602a61e
LucFERRO <luchenri.ferro@gmail.com>
2023-01-16 14:45:53 +0100

    fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,11 @@
 function Button() {
     var bulbon = document.getElementById("ON")
     var element = document.getElementsByClassName("right-section");
+    var element2 = document.getElementsByClassName("left-section");
     for(var right of element){
         right.classList.toggle("dark-mode");
     }
-    var element2 = document.getElementsByClassName("left-section");
+
     for(var left of element2){
         left.classList.toggle("dark-mode");
     }
@@ -14,6 +15,7 @@ function Button() {
         bulbon.setAttribute("src","assets/img/OFFopti.png")
         bulbon.setAttribute("title","Pas assez de lumière?")
     }
+    
     else{
         bulbon.setAttribute("src","assets/img/ONopti.png")
         bulbon.setAttribute("title","Trop de lumière?")
